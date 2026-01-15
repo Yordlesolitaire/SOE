ac641bcc5e1607a826f1a7fa4e18b8ec3d3020ea
Damien ESCUDERO <damienflash22@gmail.com>
2026-01-15 12:04:41 +0100

    Nav classes

--- src/pages/Classes.jsx
+++ src/pages/Classes.jsx
@@ -1,4 +1,7 @@
 import { useMediaQuery } from "../components/useMediaQuery"
+import { useState } from "react";
+
+
 export default function Classes(){
     const isMobile = useMediaQuery("(max-width: 768px)")
     return(
@@ -6,7 +9,7 @@ export default function Classes(){
             {isMobile ? (
                 <MobileLayout />
             ) : (
-                <h1>PC</h1>
+                <MobileLayout/>
             )}
         </>
     )
@@ -14,11 +17,19 @@ export default function Classes(){
 
 
 function MobileLayout(){
+    const [open, setOpen] = useState(false);
     return(
         <>
-            <aside>
-                <button className="nav_button"></button>
-            </aside>
+            <aside className={open ? "open" : ""}>
+                <button 
+                    className={`nav_button ${open ? "open" : ""}`} 
+                    onClick={() => setOpen(o => !o)}
+                >
+                    <div>
+                        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracurrentcolorerCarrier" stroke-linecurrentcolorap="round" stroke-linejoin="round"></g><g id="SVGRepo_icurrentcoloronCarrier"> <path fill-rule="evenodd" currentcolorlip-rule="evenodd" d="M8.29289 5.29289C8.68342 4.90237 9.31658 4.90237 9.70711 5.29289L15.5303 11.1161C16.0185 11.6043 16.0185 12.3957 15.5303 12.8839L9.70711 18.7071C9.31658 19.0976 8.68342 19.0976 8.29289 18.7071C7.90237 18.3166 7.90237 17.6834 8.29289 17.2929L13.5858 12L8.29289 6.70711C7.90237 6.31658 7.90237 5.68342 8.29289 5.29289Z" fill="currentcolor"></path> </g></svg>
+                    </div>
+                </button>
+            </aside>    
             <main></main>
         </>
     )
