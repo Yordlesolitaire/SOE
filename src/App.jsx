9dfce9aa6704c028a4200fd4faaea3a431b7c405
Damien ESCUDERO <damienflash22@gmail.com>
2026-01-16 11:06:25 +0100

    Probème de boutons

--- src/App.jsx
+++ src/App.jsx
@@ -17,17 +17,55 @@ function App() {
                 <Aside/>
                 <main className={styles.main}>
                     <Routes>
+                        {/* Accueil */}
                         <Route path="/" element={<h1>Accueil</h1>} />
-                        <Route path="/classes" element={<h1>Classes</h1>}/>
+
+                        {/* Classes */}
+                        <Route path="/classes" element={<h1>Classes</h1>} />
+                        <Route path="/classes/guerrier" element={<h1>Guerrier</h1>} />
+                        <Route path="/classes/guerrier/berserker" element={<h1>Berserker</h1>} />
+                        <Route path="/classes/mage" element={<h1>Mage</h1>} />
+                        <Route path="/classes/mage/elementaliste" element={<h1>Élémentaliste</h1>} />
+
+                        {/* Races */}
                         <Route path="/races" element={<h1>Races</h1>} />
+                        <Route path="/races/humains" element={<h1>Humains</h1>} />
+                        <Route path="/races/nains" element={<h1>Nains</h1>} />
+                        <Route path="/races/elfes" element={<h1>Elfés</h1>} />
+
+                        {/* Quêtes */}
                         <Route path="/quetes" element={<h1>Quêtes</h1>} />
+                        <Route path="/quetes/principales" element={<h1>Quêtes principales</h1>} />
+                        <Route path="/quetes/secondaires" element={<h1>Quêtes secondaires</h1>} />
+
+                        {/* Codex */}
                         <Route path="/codex" element={<h1>Codex</h1>} />
+                        <Route path="/codex/armes" element={<h1>Armes</h1>} />
+                        <Route path="/codex/armes/legendaires" element={<h1>Armes légendaires</h1>} />
+                        <Route path="/codex/armures" element={<h1>Armures</h1>} />
+                        <Route path="/codex/consommables" element={<h1>Consommables</h1>} />
+
+                        {/* Outils */}
                         <Route path="/outils" element={<h1>Outils</h1>} />
+                        <Route path="/outils/creation-personnage" element={<h1>Créateur de personnage</h1>} />
+                        <Route path="/outils/generateur-noms" element={<h1>Générateur de noms</h1>} />
+                        <Route path="/outils/des" element={<h1>Lanceur de dés</h1>} />
+
+                        {/* Historique */}
                         <Route path="/historique" element={<h1>Historique</h1>} />
+                        <Route path="/historique/chronologie" element={<h1>Chronologie</h1>} />
+                        <Route path="/historique/ages" element={<h1>Âges du monde</h1>} />
+                        <Route path="/historique/evenements" element={<h1>Événements majeurs</h1>} />
+                        <Route path="/historique/personnages" element={<h1>Personnages historiques</h1>} />
+
+                        {/* Paramètres */}
                         <Route path="/parametre" element={<h1>Paramètres</h1>} />
+
                         {/* 404 */}
                         <Route path="*" element={<h1>Page introuvable</h1>} />
-                    </Routes>   
+                    </Routes>
+
+
                 </main>
             </div>
         </>
--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -4,14 +4,143 @@ import { useMediaQuery } from "../../functions/useMediaQuery";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-// Menu dynamique
+function HomeIcon(){
+  return(<svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" fill="currentcolor"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path fill="currentcolor" d="M256 19.27L25.637 249.638 19.27 256 32 268.73l6.363-6.367L256 44.727l217.637 217.636L480 268.73 492.73 256l-6.367-6.363zM96 48v107.273l64-64.002V48zm160 20.727l-192 192V486h64V320h96v166h224V260.727zM288 320h96v80h-96z"></path></g></svg>)
+}
+
+function Arrow(){
+  return <svg height="100%" viewBox="0 0 24 24" fill="currentcolor" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M8.29289 5.29289C8.68342 4.90237 9.31658 4.90237 9.70711 5.29289L15.5303 11.1161C16.0185 11.6043 16.0185 12.3957 15.5303 12.8839L9.70711 18.7071C9.31658 19.0976 8.68342 19.0976 8.29289 18.7071C7.90237 18.3166 7.90237 17.6834 8.29289 17.2929L13.5858 12L8.29289 6.70711C7.90237 6.31658 7.90237 5.68342 8.29289 5.29289Z" fill="currentcolor"></path> </g></svg>
+}
+
 const MENU = [
-  { name: "Accueil", path: "/",
-    submenu: [],
-    icon:    <svg height={"100%"} viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" fill="currentcolor"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path fill="currentcolor" d="M256 19.27L25.637 249.638 19.27 256 32 268.73l6.363-6.367L256 44.727l217.637 217.636L480 268.73 492.73 256l-6.367-6.363zM96 48v107.273l64-64.002V48zm160 20.727l-192 192V486h64V320h96v166h224V260.727zM288 320h96v80h-96z"></path></g></svg>,
-},
-  { name: "Classes", path: "/classes", icon: null, submenu: ["Guerreir  "] },
+  {
+    name: "Accueil",
+    path: "/",
+    icon: <HomeIcon />,
+    children: [],
+  },
+
+  {
+    name: "Classes",
+    path: "/classes",
+    icon: null,
+    children: [
+      {
+        name: "Guerrier",
+        path: "/classes/guerrier",
+        children: [
+          {
+            name: "Berserker",
+            path: "/classes/guerrier/berserker",
+            children: [],
+          },
+        ],
+      },
+      {
+        name: "Mage",
+        path: "/classes/mage",
+        children: [
+          {
+            name: "Élémentaliste",
+            path: "/classes/mage/elementaliste",
+            children: [],
+          },
+        ],
+      },
+    ],
+  },
+
+  {
+    name: "Races",
+    path: "/races",
+    icon: null,
+    children: [
+      { name: "Humains", path: "/races/humains", children: [] },
+      { name: "Nains", path: "/races/nains", children: [] },
+      { name: "Elfés", path: "/races/elfes", children: [] },
+    ],
+  },
+
+  {
+    name: "Quêtes",
+    path: "/quetes",
+    icon: null,
+    children: [
+      { name: "Principales", path: "/quetes/principales", children: [] },
+      { name: "Secondaires", path: "/quetes/secondaires", children: [] },
+    ],
+  },
+
+  {
+    name: "Codex",
+    path: "/codex",
+    icon: null,
+    children: [
+      {
+        name: "Armes",
+        path: "/codex/armes",
+        children: [
+          {
+            name: "Armes légendaires",
+            path: "/codex/armes/legendaires",
+            children: [],
+          },
+        ],
+      },
+      { name: "Armures", path: "/codex/armures", children: [] },
+      { name: "Consommables", path: "/codex/consommables", children: [] },
+    ],
+  },
+
+  {
+    name: "Outils",
+    path: "/outils",
+    icon: null,
+    children: [
+      { name: "Créateur de personnage", path: "/outils/creation-personnage", children: [] },
+      { name: "Générateur de noms", path: "/outils/generateur-noms", children: [] },
+      { name: "Lanceur de dés", path: "/outils/des", children: [] },
+    ],
+  },
+
+  {
+    name: "Historique",
+    path: "/historique",
+    icon: null,
+    children: [
+      {
+        name: "Chronologie",
+        path: "/historique/chronologie",
+        children: [],
+      },
+      {
+        name: "Âges du monde",
+        path: "/historique/ages",
+        children: [],
+      },
+      {
+        name: "Événements majeurs",
+        path: "/historique/evenements",
+        children: [],
+      },
+      {
+        name: "Personnages historiques",
+        path: "/historique/personnages",
+        children: [],
+      },
+    ],
+  },
+  {
+    name: "Paramètres",
+    path: "/parametre",
+    icon: null,
+    children: [],
+  },
 ];
+
+
+
+
 
 export default function Aside() {
   const isPhone = useMediaQuery("(max-width: 768px)");
@@ -27,20 +156,91 @@ export default function Aside() {
 // Desktop Menu
 // ----------------------
 function DesktopNav() {
-  const navigate = useNavigate();
-
   return (
     <aside className={styles.aside} id={styles.aside_DESKTOP}>
-      {MENU.map((item, index) => (
-        <button key={index} className={styles.Menu} onClick={() => navigate(item.path)}>
-          <span id={styles.icon}>{item.icon}</span>
-          <span id={styles.text}><h1>{item.name}</h1></span>
-          <span id={styles.arrow}>{item.submenu.length >= 1 && <svg viewBox="0 0 24 24" fill="currentcolor" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M8.29289 5.29289C8.68342 4.90237 9.31658 4.90237 9.70711 5.29289L15.5303 11.1161C16.0185 11.6043 16.0185 12.3957 15.5303 12.8839L9.70711 18.7071C9.31658 19.0976 8.68342 19.0976 8.29289 18.7071C7.90237 18.3166 7.90237 17.6834 8.29289 17.2929L13.5858 12L8.29289 6.70711C7.90237 6.31658 7.90237 5.68342 8.29289 5.29289Z" fill="currentcolor"></path> </g></svg>}</span>
-        </button>
-      ))}
+        <MenuItem/>
     </aside>
   );
 }
+
+
+
+function MenuItem() {
+  const navigate = useNavigate();
+  
+  return(
+    <>
+      {MENU.map((item, index) => {
+          const hasChildren = item.children && item.children.length > 0;
+
+          return (
+            <button key={index} onClick={() => navigate(item.path)}>
+              <span id={styles.icon}>{item.icon}</span>
+              <span id={styles.text}><h2>{item.name}</h2></span>
+              <span id={styles.arrow}>{hasChildren && <Arrow/>}</span>
+              {hasChildren &&<div className={styles.submenu}>{<SubMenuItem menus={item.children} />}</div>}
+            </button>
+          );
+        })}
+    </>
+  )
+} 
+
+
+
+function SubMenuItem({menus}){
+  const navigate = useNavigate();
+  return(
+    
+    <>
+    
+      {menus.map((item,index) => {
+        const hasChildren = item.children && item.children.length > 0;
+        return(
+        <button key={index} onClick={() => navigate(item.path)}>
+          <span><h4>{item.name}</h4></span>
+          <span><Arrow/></span>
+        </button>
+      )
+      })}
+    </> 
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // ----------------------
 // Mobile Menu
