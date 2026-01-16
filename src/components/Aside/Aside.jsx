cdee5b31f5ef2635390e9a4090f81cc5ba535df0
Damien ESCUDERO <damienflash22@gmail.com>
2026-01-16 11:34:05 +0100

    Probème de boutons

--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -147,7 +147,7 @@ export default function Aside() {
 
   return (
     <>
-      {isPhone ? <MobileNav /> : <DesktopNav />}
+      {isPhone ? <h1>Mobile</h1> : <DesktopNav />}
     </>
   );
 }
@@ -158,115 +158,6 @@ export default function Aside() {
 function DesktopNav() {
   return (
     <aside className={styles.aside} id={styles.aside_DESKTOP}>
-        <MenuItem/>
-    </aside>
-  );
-}
-
-
-
-
-function MenuItem({ MENU }) {
-  const navigate = useNavigate();
-
-  return (
-    <>
-      {MENU.map((item, index) => (
-        <MenuNode key={index} item={item} navigate={navigate} />
-      ))}
-    </>
-  );
-}
-
-function MenuNode({ item, navigate }) {
-  const hasChildren = item.children && item.children.length > 0;
-
-  return (
-    <div className={styles.menuItem}>
-      <button className={styles.Menu} onClick={() => navigate(item.path)}>
-        <span className={styles.icon}>{item.icon}</span>
-        <span className={styles.text}>{item.name}</span>
-        {hasChildren && <span className={styles.arrow}><Arrow /></span>}
-      </button>
-
-      {hasChildren && (
-        <div className={styles.submenu}>
-          {item.children.map((child, idx) => (
-            <MenuNode key={idx} item={child} navigate={navigate} />
-          ))}
-        </div>
-      )}
-    </div>
-  );
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ----------------------
-// Mobile Menu
-// ----------------------
-function MobileNav() {
-  const [opened, setOpened] = useState(false);
-  const isPhone = useMediaQuery("(max-width: 768px)");
-  const navigate = useNavigate();
-
-  // Ferme le menu si l'écran devient desktop
-  useEffect(() => {
-    if (!isPhone) setOpened(false);
-  }, [isPhone]);
-
-  return (
-    <aside
-      className={`${styles.aside} ${opened ? styles.open : ""}`}
-      id={styles.aside_MOBILE}
-    >
-      {/* Bouton toggle */}
-      <button
-        className={`${styles.Mobile_opener} ${opened ? styles.open : ""}`}
-        onClick={() => setOpened((o) => !o)}
-      >
-        <svg
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M8.29289 5.29289C8.68342 4.90237 9.31658 4.90237 9.70711 5.29289L15.5303 11.1161C16.0185 11.6043 16.0185 12.3957 15.5303 12.8839L9.70711 18.7071C9.31658 19.0976 8.68342 19.0976 8.29289 18.7071C7.90237 18.3166 7.90237 17.6834 8.29289 17.2929L13.5858 12L8.29289 6.70711C7.90237 6.31658 7.90237 5.68342 8.29289 5.29289Z"
-            fill="currentColor"
-          />
-        </svg>
-      </button>
     </aside>
   );
 }
