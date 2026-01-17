937d01f2aa21bc25288aa620132d09077848376e
Damien ESCUDERO <damienflash22@gmail.com>
2026-01-17 15:35:50 +0100

    bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
+import "./style/variables.css";
+import "./style/reset.css";
+import "./Style.css";
 import Header from "./Components/Header/Header";
-import "./style/variables.css"
-import "./style/reset.css"
-function App() {
-    //const [asideOpen, setAsideOpen] = useState(false);
 
-    return (
-        <>
-            <Header/>
-        </>
-    );
+import useDevice from "./functions/device";
+
+function App() {
+  const device = useDevice();
+
+  return (
+    <>
+      <Header device={device}/>
+    </>
+  );
 }
 
 export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,42 +1,10 @@
-import useDevice from "../../functions/device"
-import Logo from "../Svg/Logo";
-import desk from "../Header/Desktop.module.css"
-import Portrait from "../Header/Portrait.module.css";
-import Paysage from "../Header/Paysage.module.css";
 
 
 
-export default function Header(){
-    const { isMobile, isPortrait } = useDevice();
-    return(
-        <>
-        {isMobile ? (<Mobile/>) : (<Desktop />)}
-        </>
-    )
-}
-
-function Desktop(){
-    return(
-        <header className={desk.header}>
-            <div><Logo/></div>
-            <h1>Shadows of Erdalorn</h1>
-            <div><Logo/></div>
-        </header>
-    )
-}
 
 
+export default function Header({ device }) {
+  const { isMobile, isPortrait } = device;
 
-function Mobile(){
-    const { isMobile, isPortrait } = useDevice();
-    return(
-        <>
-            <header className={isPortrait ? (Portrait.header) : (Paysage.header)}>
-                <div><Logo/></div>
-                <h1>Shadows of Erdalorn</h1>
-                <div><Logo/></div>
-            </header>
-        </>
-    )
-    
+  return isMobile ? <h1>Mobile</h1> : <h1>Desktop</h1>;
 }
