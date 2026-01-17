c76c278d26650f70608c7ab86ecabd7b71609e1e
Damien ESCUDERO <damienflash22@gmail.com>
2026-01-17 14:02:55 +0100

    Restart

--- src/App.jsx
+++ src/App.jsx
@@ -1,75 +1,12 @@
-import { Header } from "./components/Header/Header";
-import { Route,Routes } from "react-router-dom";
-import Aside from "./components/Aside/Aside";
-import "./Style.css"
-import "./style/reset.css";
-import "./style/variables.css";
-
-//import { useState } from "react";
-
+import Header from "./Components/Header/Header";
+import "./style/variables.css"
+import "./style/reset.css"
 function App() {
     //const [asideOpen, setAsideOpen] = useState(false);
 
     return (
         <>
             <Header/>
-            <div className="content">
-                <Aside/>
-                <Routes>
-                    {/* Accueil */}
-                    <Route path="/" element={<h1>Accueil</h1>} />
-
-                    {/* Classes */}
-                    <Route path="/classes" element={<h1>Classes</h1>} />
-                    <Route path="/classes/guerrier" element={<h1>Guerrier</h1>} />
-                    <Route path="/classes/chasseur" element={<h1>Chasseur</h1>} />
-                    <Route path="/classes/incarne" element={<h1>Incarné</h1>} />
-                    <Route path="/classes/lamesombre" element={<h1>Lamesombre</h1>} />
-                    <Route path="/classes/profanateur" element={<h1>Profanateur</h1>} />
-                    <Route path="/classes/artiste-martial" element={<h1>Artiste Martial</h1>} />
-                    <Route path="/classes/artificier" element={<h1>Artificier</h1>} />
-                    <Route path="/classes/mage" element={<h1>Mage</h1>} />
-                    <Route path="/classes/clerc" element={<h1>Clerc</h1>} />
-                    <Route path="/classes/barde" element={<h1>Barde</h1>} />
-
-                    {/* Races */}
-                    <Route path="/races" element={<h1>Races</h1>} />
-                    <Route path="/races/humains" element={<h1>Humains</h1>} />
-                    <Route path="/races/nains" element={<h1>Nains</h1>} />
-                    <Route path="/races/elfes" element={<h1>Elfes</h1>} />
-
-                    {/* Quêtes */}
-                    <Route path="/quetes" element={<h1>Quêtes</h1>} />
-                    <Route path="/quetes/table" element={<h1>Tableau de Quêtes</h1>} />
-                    <Route path="/quetes/selectione" element={<h1>Quêtes séléctioné</h1>} />
-
-                    {/* Codex */}
-                    <Route path="/codex" element={<h1>Codex</h1>} />
-                    <Route path="/codex/armes" element={<h1>Armes</h1>} />
-                    <Route path="/codex/armes/legendaires" element={<h1>Armes légendaires</h1>} />
-                    <Route path="/codex/armures" element={<h1>Armures</h1>} />
-                    <Route path="/codex/consommables" element={<h1>Consommables</h1>} />
-
-                    {/* Outils */}
-                    <Route path="/outils" element={<h1>Outils</h1>} />
-                    <Route path="/outils/des" element={<Dé/>} />
-
-                    {/* Historique */}
-                    <Route path="/historique" element={<h1>Historique</h1>} />
-                    <Route path="/historique/chronologie" element={<h1>Chronologie</h1>} />
-                    <Route path="/historique/ages" element={<h1>Âges du monde</h1>} />
-                    <Route path="/historique/evenements" element={<h1>Événements majeurs</h1>} />
-                    <Route path="/historique/personnages" element={<h1>Personnages historiques</h1>} />
-
-                    {/* Univers */}
-                    <Route path="/univers" element={<h1>Univers</h1>} />
-                    {/* Paramètres */}
-                    <Route path="/parametre" element={<h1>Paramètres</h1>} />
-
-                    {/* 404 */}
-                    <Route path="*" element={<h1>Page introuvable</h1>} />
-                </Routes>
-            </div>
         </>
     );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,42 @@
-//import { Link } from "react-router-dom";
-import styles from "./Header.module.css";
-import { Logo } from "../DATA/Logo";
-export function Header() {
-    return (
-        <header className={styles.header}>
-            <Logo/>
-            <h1 className={styles.title}>Shadows of Erdalorn</h1>
-            <Logo/>
+import useDevice from "../../functions/device"
+import Logo from "../Svg/Logo";
+import desk from "../Header/Desktop.module.css"
+import Portrait from "../Header/Portrait.module.css";
+import Paysage from "../Header/Paysage.module.css";
+
+
+
+export default function Header(){
+    const { isMobile, isPortrait } = useDevice();
+    return(
+        <>
+        {isMobile ? (<Mobile/>) : (<Desktop />)}
+        </>
+    )
+}
+
+function Desktop(){
+    return(
+        <header className={desk.header}>
+            <div><Logo/></div>
+            <h1>Shadows of Erdalorn</h1>
+            <div><Logo/></div>
         </header>
-    );
+    )
+}
+
+
+
+function Mobile(){
+    const { isMobile, isPortrait } = useDevice();
+    return(
+        <>
+            <header className={isPortrait ? (Portrait.header) : (Paysage.header)}>
+                <div><Logo/></div>
+                <h1>Shadows of Erdalorn</h1>
+                <div><Logo/></div>
+            </header>
+        </>
+    )
+    
 }
