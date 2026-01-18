cce45167c4f8a37b71427dabc699ca23b94feb5f
Damien ESCUDERO <damienflash22@gmail.com>
2026-01-18 10:34:39 +0100

    Nouvel aside Desktop

--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -1,7 +1,7 @@
 import Desk from "./Desktop.module.css"
 import Portrait from "./Portrait.module.css"
 import Paysage from "./Paysage.module.css"
-import { Arrow,Accueil,Classes,Settings } from "../../data/Svgs"
+import { Arrow,Accueil,Classes,Settings,Races } from "../../data/Svgs"
 
 const MENU = [
   {
@@ -18,9 +18,82 @@ const MENU = [
       {
         name: "Guerrier",
         path: "/classes/guerrier",
-        icon: null,
-        children: [],
       },
+      {
+        name: "Lamesombre",
+        path: "/classes/lamesombre",
+      },
+      {
+        name: "Chasseur",
+        path: "/classes/chasseur",
+      },
+      {
+        name: "L’Incarné",
+        path: "/classes/incarne",
+      },
+      {
+        name: "Profanateur",
+        path: "/classes/profanateur",
+      },
+      {
+        name: "Artificier",
+        path: "/classes/artificier",
+      },
+      {
+        name: "Artiste martial",
+        path: "/classes/artiste-martial",
+      },
+      {
+        name: "Mage",
+        path: "/classes/mage",
+      },
+      {
+        name: "Barde",
+        path: "/classes/barde",
+      },
+      {
+        name: "Clerc",
+        path: "/classes/clerc",
+      },
+    ],
+  },
+    {
+    name: "Races",
+    path: "/races",
+    icon: <Races/>,
+    children: [
+        {
+          name: "Fizzarains",
+          path: "/races/fizzarains",
+        },
+        {
+          name: "Goliath",
+          path: "/races/goliath",
+        },
+        {
+          name: "Nains",
+          path: "/races/nains",
+        },
+        {
+          name: "Elfes",
+          path: "/races/elfes",
+        },
+        {
+          name: "Humains",
+          path: "/races/humains",
+        },
+        {
+          name: "Orcs",
+          path: "/races/orcs",
+        },
+        {
+          name: "Drakéides",
+          path: "/races/drakeides",
+        },
+        {
+          name: "Tieffelin",
+          path: "/races/tieffelin",
+        },
     ],
   },
   {
@@ -50,18 +123,29 @@ function Desktop() {
   return (
     <aside className={Desk.aside}>
       {MENU.map((item, index) => (
-        <div key={index}>
-          <button className={Desk.button}>
+        <div key={index} className={Desk.menu} tabIndex={-1}>
+          <button className={Desk.button} tabIndex={-1}>
             <span id={Desk.icon}>{item.icon}</span>
             <span id={Desk.text}><h1>{item.name}</h1></span>
             <span id={Desk.arrow}>
               {item.children.length > 0 ? <Arrow /> : null}
             </span>
           </button>
+          {item.children.length > 0 ? <Subdesktop Children={item.children} /> : null}
         </div>
       ))}
     </aside>
   );
+}
+
+function Subdesktop({Children}){
+  return(
+    <div className={Desk.submenu}>
+      {Children.map((item,index) => (
+        <button key={index} tabIndex={-1}><h1>{item.name}</h1></button>
+      ))}
+    </div>
+  )
 }
 
 
